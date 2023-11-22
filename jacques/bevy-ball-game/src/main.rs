63dc47d890b1b880dcb28e3c574f6efc5e57f4c8
Estefan Hu <estefanhu074@gmail.com>
2023-11-21 23:37:07 -0800

    Migrated files

--- jacques/bevy-ball-game/src/main.rs
+++ jacques/bevy-ball-game/src/main.rs
@@ -1,3 +1,4 @@
+use bevy::app::AppExit;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use rand::prelude::*;
@@ -7,6 +8,7 @@ pub const PLAYER_SIZE: f32 = 64.0; // player sprite size
 pub const NUMBER_OF_ENEMIES: usize = 4;
 pub const ENEMY_SPEED: f32 = 200.0;
 pub const ENEMY_SIZE: f32 = 64.0;
+pub const ENEMY_SPAWN_TIME: f32 = 1.0;
 pub const NUMBER_OF_STARS: usize = 10;
 pub const STAR_SIZE: f32 = 30.0;
 pub const STAR_SPAWN_TIME: f32 = 1.0;
@@ -15,6 +17,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .init_resource::<Score>()
+        .init_resource::<EnemySpawnTimer>()
         .init_resource::<StarSpawnTimer>()
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
@@ -28,8 +31,11 @@ fn main() {
         .add_system(enemy_hit_player)
         .add_system(player_hit_star)
         .add_system(update_score)
+        .add_system(tick_enemy_spawn_timer)
         .add_system(tick_star_spawn_timer)
+        .add_system(spawn_enemy_over_time)
         .add_system(spawn_stars_over_time)
+        .add_system(exit_game)
         .run();
 }
 
@@ -54,6 +60,17 @@ impl Default for Score {
         Score {
             value: 0
         }
+    }
+}
+
+#[derive(Resource)]
+pub struct EnemySpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for EnemySpawnTimer {
+    fn default() -> EnemySpawnTimer {
+        EnemySpawnTimer { timer: Timer::from_seconds(ENEMY_SPAWN_TIME, TimerMode::Repeating) }
     }
 }
 
@@ -342,15 +359,43 @@ pub fn update_score(score: Res<Score>) {
     }
 }
 
+pub fn tick_enemy_spawn_timer(mut enemy_spawn_timer: ResMut<EnemySpawnTimer>, time: Res<Time>) {
+    enemy_spawn_timer.timer.tick(time.delta());
+}
+
 pub fn tick_star_spawn_timer(mut star_spawn_timer: ResMut<StarSpawnTimer>, time: Res<Time>) {
     star_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_enemy_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    enemy_spawn_timer: Res<EnemySpawnTimer>
+) {
+    if enemy_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(),
+            }
+        ));
+    }
 }
 
 pub fn spawn_stars_over_time(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
     asset_server: Res<AssetServer>,
-    mut star_spawn_timer: Res<StarSpawnTimer>
+    star_spawn_timer: Res<StarSpawnTimer>
 ) {
     if star_spawn_timer.timer.finished() {
         let window = window_query.get_single().unwrap();
@@ -365,5 +410,14 @@ pub fn spawn_stars_over_time(
             },
             Star {}
         ));
+    }
+}
+
+pub fn exit_game(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut app_exit_event_writer: EventWriter<AppExit>
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        app_exit_event_writer.send(AppExit);
     }
 }
